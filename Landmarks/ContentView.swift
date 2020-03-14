a0167003ac0727979c41d754245603b0bea789ef
Robert Gravina <robert.gravina@gmail.com>
2020-03-14 15:50:05 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Robert Gravina on 2020/03/14.
+//  Copyright © 2020 Robert Gravina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
