d3efeb31154390a8991ed8a90fb338bda5d1bbbc
Robert Gravina <robert.gravina@gmail.com>
2020-03-16 17:34:24 +0900

    Allow user to favourite and unfavourite landmarks.

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -11,7 +11,7 @@ struct LandmarkList: View {
                 }
                 ForEach(userData.landmarks) { landmark in
                     if !self.userData.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark).environmentObject(self.userData)) {
                             LandmarkRow(landmark: landmark)
                         }
                     }
