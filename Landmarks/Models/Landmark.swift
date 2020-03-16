07fffa4648306169b2c0cf3a07f2e4c0284a0d4b
Robert Gravina <robert.gravina@gmail.com>
2020-03-16 15:22:53 +0900

    Updated changes at start of next tutorial. Show favourite icon.

--- Landmarks/Models/Landmark.swift
+++ Landmarks/Models/Landmark.swift
@@ -9,6 +9,7 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var park: String
     var category: Category
+    var isFavorite: Bool
 
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
@@ -20,6 +21,7 @@ struct Landmark: Hashable, Codable, Identifiable {
         case featured = "Featured"
         case lakes = "Lakes"
         case rivers = "Rivers"
+        case mountains = "Mountains"
     }
 }
 
