8cb18a76e25a50adbcf30c3fbbf3c0ff0fd08580
Robert Gravina <robert.gravina@gmail.com>
2020-03-16 11:15:47 +0900

    Add LandmarkRow view.

--- Landmarks/Models/Data.swift
+++ Landmarks/Models/Data.swift
@@ -1,10 +1,3 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-Helpers for loading images and data.
-*/
-
 import UIKit
 import SwiftUI
 import CoreLocation
--- Landmarks/Models/Landmark.swift
+++ Landmarks/Models/Landmark.swift
@@ -1,10 +1,3 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-The model for an individual landmark.
-*/
-
 import SwiftUI
 import CoreLocation
 
