44791aab536c9eabdb13cea72b999b95b2d2f8d1
Robert Gravina <robert.gravina@gmail.com>
2020-03-16 17:16:25 +0900

    Use observable for user data.

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -1,16 +1,16 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State var showFavoritesOnly = false
+    @EnvironmentObject var userData: UserData
 
     var body: some View {
         NavigationView {
             List {
-                Toggle(isOn: $showFavoritesOnly) {
+                Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
                 }
-                ForEach(landmarkData) { landmark in
-                    if !self.showFavoritesOnly || landmark.isFavorite {
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
                         NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                             LandmarkRow(landmark: landmark)
                         }
@@ -24,9 +24,7 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-        }
+        LandmarkList()
+            .environmentObject(UserData())
     }
 }
