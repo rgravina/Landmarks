0b79cc3cfcfbf8fabd5d14700a5d4eafa9059551
Robert Gravina <robert.gravina@gmail.com>
2020-03-29 15:05:07 +0900

    Add empty badge view.

---
+++ Landmarks/Badge.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
