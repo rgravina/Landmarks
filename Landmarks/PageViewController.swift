aba7db85dc8c45a004374da013ce0712ea1e17a2
Robert Gravina <robert.gravina@gmail.com>
2020-04-14 22:09:27 +0900

    Create page view controller data source.

--- Landmarks/PageViewController.swift
+++ Landmarks/PageViewController.swift
@@ -4,6 +4,10 @@ import UIKit
 struct PageViewController: UIViewControllerRepresentable {
     var controllers: [UIViewController]
 
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
@@ -18,5 +22,40 @@ struct PageViewController: UIViewControllerRepresentable {
             direction: .forward,
             animated: true
         )
+        pageViewController.dataSource = context.coordinator
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return parent.controllers.last
+            }
+            return parent.controllers[index - 1]
+        }
+
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == parent.controllers.count {
+                return parent.controllers.first
+            }
+            return parent.controllers[index + 1]
+        }
     }
 }
